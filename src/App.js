751f6d3c847f44ddc69c92378c0091e061de476f
nick-kuhn88 <nick.kuhn88@gmail.com>
2020-04-08 11:19:41 -0700

    added list and note components to the app. They are included in the app markup.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import List from './components/List';
+import Note from './components/Note';
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <List />
+        <Note />
+      </div>
+    );
+  }
 }
 
 export default App;
