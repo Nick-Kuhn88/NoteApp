41e1ca76fdeb2ffebda5e242837fdd56f8a708c8
nick-kuhn88 <nick.kuhn88@gmail.com>
2020-04-09 15:23:59 -0700

    connected to the Rails API to load the notes data. Created a helper to write the function that connects to the endpoint for the data.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
 import axios from 'axios';
+import urlFor from './helpers/urlFor';
 
 class App extends React.Component {
   constructor() {
@@ -20,7 +21,7 @@ class App extends React.Component {
   }
 
   getNotes = () => {
-    axios.get('https://firehose-note-api.herokuapp.com/notes')
+    axios.get(urlFor('notes'))
     .then((res) => console.log(res.data) )
     .catch((err) => console.log(err.response.data) );
   }
