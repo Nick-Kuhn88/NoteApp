655576a31ec69658c170c5bc44f9535945d497da
nick-kuhn88 <nick.kuhn88@gmail.com>
2020-04-09 15:03:48 -0700

    connected the back end API and passed the received data through as props to the list page.

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 class List extends React.Component {
+
+  componentWillMount() {
+    this.props.getNotes();
+  }
+
   render() {
     return (
       <div className="list-container">
