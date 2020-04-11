b0be534013ee358dfd9b19eccfa56a249381a1c7
nick-kuhn88 <nick.kuhn88@gmail.com>
2020-04-11 11:57:55 -0700

    app can now successfully create and update notes through proper HTTP channels.

--- src/App.js
+++ src/App.js
@@ -34,8 +34,16 @@ class App extends React.Component {
     .catch((err) => console.log(err.response.data) );
   }
 
-  submitNote = (data) => {
-    axios.post(urlFor('notes'), data)
+  performSubmissionRequest = (data, id) => {
+    if (id) {
+      return axios.patch(urlFor(`notes/${id}`), data);
+    } else {
+      return axios.post(urlFor('notes'), data);
+    }
+  }
+
+  submitNote = (data, id) => {
+    this.performSubmissionRequest(data, id)
     .then((res) => this.setState({ showNote: false }) )
     .catch((err) => console.log(err.response.data) );
   }
